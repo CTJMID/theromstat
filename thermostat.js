e29ed96c29dbc85b18c913ad0d3d822ccce17131
Chris Middleton <ctjmiddleton@gmail.com>
2022-03-03 15:30:01 +0000

    First 5 specs implemented and tested for

--- thermostat.js
+++ thermostat.js
@@ -1,13 +1,28 @@
 
+class Thermostat {
+    constructor() {
+        this.temperature = 20;
+        this.psm = true;
+    };
 
+    getTemp() {
+        return this.temperature;
+    };
 
+    up() {
+        this.temperature++;
+    };
 
+    down() {
+        if(this.temperature > 10) {
+            this.temperature--;
+        }; 
+    };
 
-
-
-
-
-
+    setPSM(value) {
+        this.psm = value;
+    };
+};
 
 
 
--- thermostat.test.js
+++ thermostat.test.js
@@ -1,1 +1,43 @@
+
+const { expect, it } = require('@jest/globals');
+const { camelCase } = require('yargs-parser');
 const Thermostat = require('./thermostat');
+
+describe("Thermostat", () => {
+    it("starts at 20 deg", () => {
+        const thermostat = new Thermostat;
+        expect(thermostat.getTemp()).toEqual(20);
+    });
+
+    it("increases the temp", () => {
+        const thermostat = new Thermostat;
+        thermostat.up();
+        expect(thermostat.getTemp()).toEqual(21);
+    });
+
+    it("decreases the temp", () => {
+        const thermostat = new Thermostat;
+        thermostat.down();
+        expect(thermostat.getTemp()).toEqual(19);
+    });
+
+    it("cant go below 10 degrees", () => {
+        const thermostat = new Thermostat;
+        // will try to descrease temp from 20 to 9
+        for(i = 0; i < 11; i++){ 
+            thermostat.down();
+        };
+        expect(thermostat.getTemp()).toEqual(10);
+    });
+
+    it("starts with PSM on", () => {
+        const thermostat = new Thermostat;
+        expect(thermostat.psm).toEqual(true);
+    });
+
+    it("turns PSM off", () => {
+        const thermostat = new Thermostat;
+        thermostat.setPSM(false);
+        expect(thermostat.psm).toEqual(false);
+    });
+});
